7f3cf0509c218759624a132c9956c9a32f31a8d0
Yuting Wang <yttfwang@berkeley.edu>
2015-10-26 20:36:05 -0700

    can damage and destroy pokemon now

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :trainers
   resources :trainers
   patch "capture", to: "pokemon#capture", as: "capture"
+  patch "damage", to: "pokemon#damage", as: "damage"
+  get "damage", to:"pokemon#damage", as: "damage2"
 end
